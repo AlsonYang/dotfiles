0e0fc9949c29edd46d7c7b75a11d20b96774d6cf
AlsonYang <31328407+AlsonYang@users.noreply.github.com>
2021-04-25 15:59:31 +1000

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -6,17 +6,30 @@ else
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew cask install AppCleaner
-brew cask install zazu
-brew cask install pock
-brew cask install IINA
-brew cask install vanilla
-brew cask install scroll-reverser
-brew cask install iterm2
-brew cask install sourcetree
-brew cask install visual-studio-code
-brew cask install qlstephen qlcolorcode qlmarkdown quicklook-json qlimagesize
+# Essential
+brew install --cask google-chrome
+brew install --cask wechat
+brew install --cask qqinput
+brew install --cask youdaodict
+brew install --cask scroll-reverser
 
+# functional
+brew install --cask AppCleaner
+brew install --cask zazu
+brew install --cask pock
+brew install --cask IINA
+brew install --cask vanilla
+brew install --cask steermouse
+brew install --cask notion
+
+# DS
+brew install --cask iterm2
+brew install --cask sourcetree
+brew install --cask visual-studio-code
+brew install --cask qlstephen qlcolorcode qlmarkdown quicklook-json qlimagesize
+brew install --cask jupyter-notebook-viewer
+
+# bash packages
 brew install wget
 brew install zsh
 brew install tldr
