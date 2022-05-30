7b02731ecc02584966b91ce90be8dd3cc359009f
Jiaming Yang <jiaming.yang@anz.com>
2022-05-30 18:57:18 +1000

    update brew packages

--- brew.sh
+++ brew.sh
@@ -12,28 +12,30 @@ fi
 # Essential
 brew install --cask google-chrome
 brew install --cask wechat
-brew install --cask sogouinput
 brew install --cask youdaodict
 
-# functional
+# # functional
 brew install --cask AppCleaner
 brew install --cask zazu
-brew install --cask pock
-brew install --cask IINA
 brew install --cask vanilla
 brew install --cask steermouse
 brew install --cask notion
-brew install --cask thunder
+brew install --cask rectangle
+brew install --cask caffeine
 
-# DS
+# # DS
 brew install --cask iterm2
 brew install --cask sourcetree
 brew install --cask visual-studio-code
 brew install --cask qlstephen qlcolorcode qlmarkdown quicklook-json qlimagesize
 brew install --cask jupyter-notebook-viewer
 
+# personal
+# brew install --cask IINA
+# brew install --cask thunder
+# brew install --cask sogouinput
+
 # bash packages
 brew install wget
-brew install zsh
 brew install tldr
 brew install htop
